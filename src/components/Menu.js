6fb248a86db10b5e43f68a29a319bcd9d69d44e4
Eveshonyanime Ocheche-Alabi <evesho.alabi@gmail.com>
2022-12-05 08:17:40 +0200

    fixed tailwind issue

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import WeeklyMenu from "../constants/MenuAPI"
+import {WeeklyMenu} from "../constants/MenuAPI"
 
 function Menu() {
 
@@ -13,7 +13,6 @@ function Menu() {
 
   return (
     <>
-
         // menu of the day items 
         {menuOfTheDay.map(({ meal }) => (
         <div className="flex items-center m-2 mt-5 space-x-2 rounded-xl cursor-pointer hover:bg-gray-200 bg-gray-100 p-3 hover:scale-105 transition transform duration-200 ease-out">
@@ -22,7 +21,6 @@ function Menu() {
             </div>
         </div>
       ))}
-
       // next button 
       <button class="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded">
         Next
